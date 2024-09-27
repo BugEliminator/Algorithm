fb927cf893ea7a6eed0c9e6ee455b495b8063cea
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2024-09-27 09:50:33 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,12 @@
+function solution(n){
+    var answer = 0;
+    let box = String(n).split("")
+    
+    for(let x of box) {
+        if(true) {
+            answer += Number(x)
+        }
+    }
+    
+    return answer;
+}
