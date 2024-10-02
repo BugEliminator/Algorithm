c656bdef38339c4cc96743def96c45bffb9a24f0
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2024-10-02 15:54:26 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.96 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,20 @@
+function solution(numbers) {
+    var answer = [];
+    let box = []
+    
+    for (let i = 0; i < numbers.length; i++) {
+        for(let j = 0; j < numbers.length; j++) {
+            if(i !== j) {
+                box.push(numbers[i] + numbers[j])
+            } 
+        }
+    }
+     let result = [...new Set(box)]
+    
+    return result.sort((a, b) => a - b);
+}
+
+
+
+// 1. 2중 for문을 사용해서 모든 경우의 수를 구해서 새로운 배열a에 저장
+// 2. a배열에서 중복된 값을 삭제하고 sort()사용
