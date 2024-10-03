4a7a297a2041b19aba407a1458945e8564a1a8fa
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2024-10-03 13:55:03 +0900

    [level 1] Title: 내적, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/70128. 내적/내적.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    let answer = 0;
+    
+    for(let i = 0; i < a.length; i++) {
+        answer += a[i] * b[i]
+    }
+    
+    return answer;
+}
+
+
+
+// -3 -2 0 8 = 3
