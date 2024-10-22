0a74da1045f6a37f2e3088e24148910fb852a5bd
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2024-10-22 14:15:04 +0900

    [level 1] Title: 폰켓몬, Time: 0.59 ms, Memory: 34.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,26 @@
+function solution(nums) {
+    var answer = 0;
+    let result    
+    let a = nums.length / 2 // 골라야 하는 수
+    let b = [...new Set(nums)].length // nums에서 중복지운값
+    result = a > b ? result = b : result =  a
+    return result
+}
+
+
+// [3', 1, 2, 3]
+
+/*
+3' 1
+3' 2
+3' 3
+1 2
+1 3
+2 3
+
+1번: 3   /   자리2
+2번: 3   /   자리3
+3번: 2   /   자리3
+[1,2,2,2,3,3,5,5,7,7] 5   / 자리 10   답 5
+
+*/
