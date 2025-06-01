060572ffec31c4f89aa5a7c541d10f50577655e1
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2025-06-01 23:01:28 +0900

    요세푸스 문제

--- JavaScript/책&강의문제/팝/요세푸스 문제.js
+++ JavaScript/책&강의문제/팝/요세푸스 문제.js
@@ -16,11 +16,6 @@ function solution(N, K) {
 console.log(solution(5, 2));
 
 /*
-  * 메모장
-
-*/
-
-/*
   * 문제 설명
   N명의 사람이 원 형태로 서 있습니다. 각 사람은 1부터 N까지 번호표를 갖고 있다.
   그리고 임의의 숫자 K가 주어졌을 때 다음과 같이 사람을 없앤다.
@@ -37,4 +32,5 @@ console.log(solution(5, 2));
   N = 6, K = 5 => result = 6
   N = 7, K = 3 => result = 1
   N = 10, K = 4 => result = 9
-*/
+  
+  */
