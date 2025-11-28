59790ecb749f72db27b1b3aea33679d3208482c1
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2025-11-28 22:32:06 +0900

    [level 0] Title: 배열 만들기 6, Time: 8.83 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,23 @@
+function solution(arr) {
+    let stk = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(stk.length === 0) {
+            stk.push(arr[i])
+        } else if(arr[i] === stk[stk.length - 1]){
+            stk.pop()
+        } else {
+            stk.push(arr[i])
+        }
+    }
+    
+    return stk.length === 0 ? [-1] : stk;
+}
+
+/*
+0 -> [0]
+1 -> [0, 1]
+2 -> [0]
+3 -> [0, 1]
+4 -> [0 ,1 ,0]
+*/
